f45186bc5483f8bfc9fd1e4224cc6b7a80f4a27a
fujinov <98008144+fujinov@users.noreply.github.com>
2022-11-27 18:09:38 +0900

    パドルの追加

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 480;
 canvas.height = 360;
 const startButton = document.getElementById("start");
+let rightPressed = false;
+let leftPressed = false;
+let gameOver = false;
 
 const ball = {
   x: canvas.width / 2,
@@ -18,6 +21,40 @@ const ball = {
     ctx.fill();
     ctx.closePath();
   },
+};
+
+const paddle = {
+  height: 10,
+  width: 75,
+  x: (canvas.width - 75) / 2,
+  dx: 6,
+
+  draw() {
+    ctx.beginPath();
+    ctx.rect(this.x, canvas.height - this.height, this.width, this.height);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  },
+};
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
 }
 
 lanchTheGame();
@@ -25,23 +62,51 @@ lanchTheGame();
 function lanchTheGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ball.draw();
+  paddle.draw();
   wallCollision();
+  paddleCollision();
   ball.x += ball.dx;
   ball.y += ball.dy;
-  requestAnimationFrame(lanchTheGame);
+  if (rightPressed) {
+    paddle.x += paddle.dx;
+    if (paddle.x + paddle.width > canvas.width) {
+      paddle.x = canvas.width - paddle.width;
+    }
+  }
+  if (leftPressed) {
+    paddle.x -= paddle.dx;
+    if (paddle.x < 0) {
+      paddle.x = 0;
+    }
+  }
+  if (gameOver) {
+    return;
+  } else {
+    requestAnimationFrame(lanchTheGame);
+  }
 }
 
 function wallCollision() {
   if (
     ball.x + ball.dx + ball.radius > canvas.width ||
     ball.x + ball.dx < ball.radius
-    ) {
-      ball.dx = -ball.dx;
-    }
-  if (
-    ball.y + ball.dy + ball.radius > canvas.height ||
-    ball.y + ball.dy < ball.radius
   ) {
+    ball.dx = -ball.dx;
+  }
+  if (ball.y + ball.dy < ball.radius) {
     ball.dy = -ball.dy;
+  }
+}
+
+function paddleCollision() {
+  if (ball.y + ball.radius >= canvas.height - paddle.height) {
+    if (
+      paddle.x - ball.radius <= ball.x &&
+      ball.x <= paddle.x + paddle.width + ball.radius
+    ) {
+      ball.dy = -ball.dy;
+    } else {
+      gameOver = true;
+    }
   }
 }
